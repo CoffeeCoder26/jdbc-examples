d74d487e8e24078833330c2af228801851a85c54
Karim Abo Agiza <60415348+KarimAgiza@users.noreply.github.com>
2021-08-07 15:36:04 +0200

    Add files via upload

---
+++ DatabaseConnection.java
@@ -0,0 +1,34 @@
+// Database Connection In JDBC
+package jdbcdemo;
+
+import java.sql.*;
+
+public class DatabaseConnection {
+
+    public static Connection getConnection(String URL, String UserName, String Pass) throws SQLException {
+        
+        Connection myConnection = DriverManager.getConnection(URL, UserName, Pass);
+        myConnection.setAutoCommit(true);
+        return myConnection;
+
+    }
+
+    public static void main(String[] args) throws SQLException {
+        Connection myConnection = null;
+        String URL = "jdbc:mysql://localhost:3306/TestDB";
+        String UserName = "root";
+        String Pass = "";
+        
+        try {
+            
+            myConnection = DatabaseConnection.getConnection(URL, UserName, Pass);
+            System.out.println("Database Is Connected ....");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            myConnection.close();
+        }
+    }
+
+}
